1f1cdc5e21ca0b05e8eefb4f8edf5013dfb1eadf
Razpberry <63367357+Razpberry@users.noreply.github.com>
2022-03-02 09:01:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ var nextclass3;
 var currentclasssentence = "The class you have right now is:";
 var currentday = "It is currently a day:";
 
+var nextclasswhen;
+
 var day1 = [
   "Math", // 8:50 - 9:39
   "Math", // 9:40 - 10:29
@@ -107,74 +109,121 @@ if (dayoftheweek == 6 || dayoftheweek == 0) {
   }
 }
 
+function setAllEmpty() {
+  nextclass1 = "";
+  nextclass2 = "";
+  nextclass3 = "";
+  next3classes = "";
+  nextclasswhen = "";
+}
+
 function getPeriod(hrs, mins) {
   var timestamp = hrs * 100 + parseInt(mins);
   if (dayoftheweek == 6 || dayoftheweek == 0) {
     curclass = "It's a weekend!";
-    nextclass1 = "";
-    nextclass2 = "";
-    nextclass3 = "";
-    next3classes = "";
+    setAllEmpty();
   } else if (timestamp >= 850 && timestamp < 940) {
     curclass = days[curdaynum][0];
     nextclass1 = days[curdaynum][1];
     nextclass2 = days[curdaynum][2];
     nextclass3 = days[curdaynum][3];
     currentclasssentence = "The class you have right now is:";
+    if (940 - timestamp - 40 < 0) {
+      nextclasswhen = 940 - timestamp
+    } else {
+      nextclasswhen = 940 - timestamp - 40
+    }
   } else if (timestamp >= 940 && timestamp < 1030) {
     curclass = days[curdaynum][1];
     nextclass1 = days[curdaynum][2];
     nextclass2 = days[curdaynum][3];
     nextclass3 = days[curdaynum][4];
     currentclasssentence = "The class you have right now is:";
+    if ((1030 - timestamp) > 60) {
+      nextclasswhen = 1030 - timestamp - 40
+    } else {
+      nextclasswhen = 1030 - timestamp
+    }
   } else if (timestamp >= 1030 && timestamp < 1050) {
     curclass = days[curdaynum][2];
     nextclass1 = days[curdaynum][3];
     nextclass2 = days[curdaynum][4];
     nextclass3 = days[curdaynum][5];
     currentclasssentence = "The class you have right now is:";
+    if ((1050 - timestamp) > 60) {
+      nextclasswhen = 1050 - timestamp - 40
+    } else {
+      nextclasswhen = 1050 - timestamp
+    }
   } else if (timestamp >= 1050 && timestamp < 1140) {
     curclass = days[curdaynum][3];
     nextclass1 = days[curdaynum][4];
     nextclass2 = days[curdaynum][5];
     nextclass3 = days[curdaynum][6];
     currentclasssentence = "The class you have right now is:";
+    if ((1140 - timestamp) > 60) {
+      nextclasswhen = 1140 - timestamp - 40
+    } else {
+      nextclasswhen = 1140 - timestamp
+    }
   } else if (timestamp >= 1140 && timestamp < 1210) {
     curclass = days[curdaynum][4];
     nextclass1 = days[curdaynum][5];
     nextclass2 = days[curdaynum][6];
     nextclass3 = days[curdaynum][7];
     currentclasssentence = "The class you have right now is:";
-  } else if (timestamp >= 1210 && timestamp < 1241) {
+    if ((1210 - timestamp) > 60) {
+      nextclasswhen = 1210 - timestamp - 40
+    } else {
+      nextclasswhen = 1210 - timestamp
+    }
+  } else if (timestamp >= 1210 && timestamp < 1331) {
     curclass = days[curdaynum][5];
     nextclass1 = days[curdaynum][6];
     nextclass2 = days[curdaynum][7];
     nextclass3 = days[curdaynum][8];
     currentclasssentence = "The class you have right now is:";
+    if ((1331 - timestamp) > 60) {
+      nextclasswhen = 1331 - timestamp - 40
+    } else {
+      nextclasswhen = 1331 - timestamp
+    }
   } else if (timestamp >= 1241 && timestamp < 1331) {
     curclass = days[curdaynum][6];
     nextclass1 = days[curdaynum][7];
     nextclass2 = days[curdaynum][8];
     nextclass3 = days[curdaynum][9];
     currentclasssentence = "The class you have right now is:";
+    if ((1331 - timestamp) > 60) {
+      nextclasswhen = 1331 - timestamp - 40
+    } else {
+      nextclasswhen = 1331 - timestamp
+    }
   } else if (timestamp >= 1331 && timestamp < 1421) {
     curclass = days[curdaynum][7];
     nextclass1 = days[curdaynum][8];
     nextclass2 = days[curdaynum][9];
     nextclass3 = "There's nothing else!";
     currentclasssentence = "The class you have right now is:";
+    if ((1421 - timestamp) > 60) {
+      nextclasswhen = 1421 - timestamp - 40
+    } else {
+      nextclasswhen = 1421 - timestamp
+    }
   } else if (timestamp >= 1421 && timestamp <= 1511) {
     curclass = days[curdaynum][8];
     nextclass1 = days[curdaynum][9];
     nextclass2 = "There's nothing else!";
     nextclass3 = "";
     currentclasssentence = "The class you have right now is:";
+    if (timestamp >= 1500) {
+      nextclasswhen = 1511 - timestamp
+    } else {
+      nextclasswhen = 1511 - timestamp - 40
+    }
   } else {
     curclass = "You're not supposed to be in school right now?!";
-    nextclass1 = "";
-    nextclass2 = "";
-    nextclass3 = "";
-    next3classes = "";
+    setAllEmpty()
   }
   return curclass;
 }
@@ -209,7 +258,10 @@ if (curdaynum == "It's a weekend!") {
 }
 
 document.getElementById("class").innerHTML = getPeriod(hours, minutes);
-
+if (nextclasswhen != "") {
+  nextclasswhen = "Your next class is in " + nextclasswhen + " minutes";
+}
+document.getElementById("nextclasswhen").innerHTML = nextclasswhen;
 document.getElementById("next3classes").innerHTML = next3classes;
 
 document.getElementById("nextclass1").innerHTML = nextclass1;
